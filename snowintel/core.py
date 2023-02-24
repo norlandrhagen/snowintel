ec3cec95a03c85cb1eaadf7e04ea6763318ba5d7
Raphael Hagen <norlandrhagen@gmail.com>
2023-02-23 20:10:38 -0700

    mapping with folium

--- snowintel/core.py
+++ snowintel/core.py
@@ -14,6 +14,7 @@ class GetSites:
     """
 
     dataframe: pd.DataFrame = None  # Assigned in __post__init__
+    geodataframe = None
     state_filter: str | list = None
     minimum_elevation: str | list = None
     maximum_elevation: str | list = None
@@ -65,10 +66,89 @@ class GetSites:
                 'Geopandas is not installed. Please install with conda/mamba "conda install -c conda-forge geopandas"'
             )
 
-        return gpd.GeoDataFrame(
+        gdf = gpd.GeoDataFrame(
             self.dataframe,
             geometry=gpd.points_from_xy(self.dataframe.longitude, self.dataframe.latitude),
         )
+        gdf.crs = "EPSG:4326"
+        self.geodataframe = gdf
+        return gdf
+
+    def create_map(self, basemap="google_terrain"):
+        try:
+            import folium
+        except Exception:
+            raise ImportError("Folium is not installed. Please install with conda/mamba/pip.")
+
+        basemaps = {
+            "google_maps": folium.TileLayer(
+                tiles="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+                attr="Google",
+                name="Google Maps",
+                overlay=True,
+                control=True,
+            ),
+            "google_satellite": folium.TileLayer(
+                tiles="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+                attr="Google",
+                name="Google Satellite",
+                overlay=True,
+                control=True,
+            ),
+            "google_terrain": folium.TileLayer(
+                tiles="https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}",
+                attr="Google",
+                name="Google Terrain",
+                overlay=True,
+                control=True,
+            ),
+            "google_satellite_hybrid": folium.TileLayer(
+                tiles="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+                attr="Google",
+                name="Google Satellite",
+                overlay=True,
+                control=True,
+            ),
+            "esri_satellite": folium.TileLayer(
+                tiles="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+                attr="Esri",
+                name="Esri Satellite",
+                overlay=True,
+                control=True,
+            ),
+        }
+
+        m = folium.Map()
+
+        basemaps[basemap].add_to(m)
+
+        geo_df_list = [[point.xy[1][0], point.xy[0][0]] for point in self.geodataframe.geometry]
+
+        for i, coords in enumerate(geo_df_list):
+            m.add_child(
+                folium.CircleMarker(
+                    location=coords,
+                    radius=10,  # in meters
+                    fill_color="#f25c3c",
+                    color="#f25c3c",
+                    popup="site_code: "
+                    + str(self.geodataframe.site_code.iloc[i])
+                    + "<br>"
+                    + "elevation: "
+                    + str(self.geodataframe.elevation_m.iloc[i])
+                    + "<br>"
+                    + "Coordinates: "
+                    + str(coords),
+                )
+            )
+
+        # folium.GeoJson(data=self.geodataframe["geometry"]).add_to(m)
+
+        sw = self.geodataframe[["latitude", "longitude"]].min().values.tolist()
+        ne = self.geodataframe[["latitude", "longitude"]].max().values.tolist()
+        m.fit_bounds([sw, ne])
+
+        return m
 
 
 @dataclass
